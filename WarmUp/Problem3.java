345d90b500b4321b0a739356112c1e877d78c5ba
meghrxj <hello.meghrajjj@gmail.com>
2021-10-17 20:21:51 +0530

    .

---
+++ WarmUp/Problem3.java
@@ -0,0 +1,24 @@
+package WarmpUp;
+
+public class Problem3 {
+
+    public static int sumDouble(int a, int b) {
+        // Store the sum in a local variable
+        int sum = a + b;
+        
+        // Double it if a and b are the same
+        if (a == b) {
+          sum = sum * 2;
+        }
+        
+        return sum;
+      }
+
+    public static void main(String[] args) {
+
+        System.out.println(sumDouble(3, 2));
+        System.out.println(sumDouble(2, 2));
+
+    }
+    
+}

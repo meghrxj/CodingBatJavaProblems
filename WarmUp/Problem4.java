d22abf93069e50ee0c04e2f901d3095bb0dc11d3
meghrxj <hello.meghrajjj@gmail.com>
2021-10-17 20:22:05 +0530

    4

---
+++ WarmUp/Problem4.java
@@ -0,0 +1,20 @@
+package WarmpUp;
+
+public class Problem4 {
+
+    public static int diff21(int n){
+        
+       if(n <= 21){
+           return 21 - n;
+
+       } else {
+           return(n - 21);
+       }
+    }
+    public static void main(String[] args) {
+
+        System.out.println(diff21(22));
+        System.out.println(diff21(18));
+    }
+    
+}

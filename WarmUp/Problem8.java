c480769cf41be444e99e1e7d409db9a8069ad165
meghrxj <hello.meghrajjj@gmail.com>
2021-10-17 20:23:14 +0530

    8

---
+++ WarmUp/Problem8.java
@@ -0,0 +1,24 @@
+package WarmpUp;
+
+public class Problem8 {
+
+    public static boolean posNeg(int a, int b, boolean negative) {
+
+        if (negative) {
+            return (a < 0 && b < 0);
+          }
+          else {
+            return ((a < 0 && b > 0) || (a > 0 && b < 0));
+          }
+        }
+
+    public static void main(String[] args){
+
+        System.out.println(posNeg(1, -1, false));
+        System.out.println(posNeg(-1, 1, false));
+        System.out.println(posNeg(-4, -5, true));
+
+    }
+}
+    
+

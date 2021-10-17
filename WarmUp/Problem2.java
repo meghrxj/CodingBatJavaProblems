74a01825412330566f8ba09a77c7482e2131b837
meghrxj <hello.meghrajjj@gmail.com>
2021-10-17 20:21:41 +0530

    .

---
+++ WarmUp/Problem2.java
@@ -0,0 +1,22 @@
+package WarmpUp;
+
+public class Problem2 {
+    
+    public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        
+        if(aSmile && bSmile) {
+            return true;
+        }
+        if(!aSmile && !bSmile) {
+            return true;
+        }
+
+        return false;
+
+  
+    }
+    public static void main(String[] args){
+        System.out.println(monkeyTrouble(false, true));
+    }
+    
+}

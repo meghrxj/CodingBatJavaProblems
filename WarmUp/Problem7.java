f3ebe67270edeaf68db3f8fa8cae6675a7c6f1a0
meghrxj <hello.meghrajjj@gmail.com>
2021-10-17 20:23:00 +0530

    7

---
+++ WarmUp/Problem7.java
@@ -0,0 +1,18 @@
+package WarmpUp;
+
+public class Problem7 {
+
+    public static boolean nearHundred(int n) {
+
+        return( (Math.abs(100 - n) <= 10) || 
+                (Math.abs(200 - n) <= 10 ) ); 
+    }
+
+    public static void main(String[] args){
+    
+        System.out.println(nearHundred(93));
+        System.out.println(nearHundred(90));
+        System.out.println(nearHundred(89));
+    }
+    
+}

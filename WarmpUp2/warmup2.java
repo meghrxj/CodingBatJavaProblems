f756cb6697a9d14d7cef62e08dfb1e6074b2a5af
meghrxj <hello.meghrajjj@gmail.com>
2021-10-18 10:27:48 +0530

    Update warmup2.java

--- WarmpUp2/warmup2.java
+++ WarmpUp2/warmup2.java
@@ -9,6 +9,16 @@ stringTimes("Hi", 2) → "HiHi"
 stringTimes("Hi", 3) → "HiHiHi"
 stringTimes("Hi", 1) → "Hi" */
 
+public static String stringTimes(String str, int n) {
+
+    String s = "";
+
+    for (int i = 0; i < n; i++){
+        s = s + str;
+
+    } return s;
+  
+}
 
 
 
@@ -26,7 +36,13 @@ stringTimes("Hi", 1) → "Hi" */
 
 
 
+public static void main(String[] args){
 
+    //stringTimes
+    System.out.println(stringTimes(("Hi", 2));
+    System.out.println(stringTimes(("Hi", 3));
+    System.out.println(stringTimes(("Hi", 1));
 
-    
+    }
+
 }

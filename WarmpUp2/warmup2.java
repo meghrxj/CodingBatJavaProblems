87e6b7f320cf0177675fa0479f2033b2305bf79e
meghrxj <hello.meghrajjj@gmail.com>
2021-10-18 13:50:00 +0530

    Update warmup2.java

--- WarmpUp2/warmup2.java
+++ WarmpUp2/warmup2.java
@@ -82,16 +82,48 @@ public static boolean doubleX(String str){
 }
 /*
 Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
-
-
 stringBits("Hello") → "Hlo"
 stringBits("Hi") → "H"
 stringBits("Heeololeo") → "Hello"*/
-public static String strinBits(String str){
+public static String stringBits(String str){
 
-
+  String result = "";
+  // Note: the loop increments i by 2
+  for (int i=0; i<str.length(); i+=2) {
+    result = result + str.substring(i, i+1);
+    // Alternately could use str.charAt(i)
+  }
+  return result;
 
 }
+
+/*
+Given a non-empty string like "Code" return a string like "CCoCodCode".
+stringSplosion("Code") → "CCoCodCode"
+stringSplosion("abc") → "aababc"
+stringSplosion("ab") → "aab"*/
+public static String stringSplosion(String str){
+
+  String result = "";
+  // On each iteration, add the substring of the chars 0..i
+  for (int i=0; i<str.length(); i++) {
+    result = result + str.substring(0, i+1);
+  }
+  return result;
+
+}
+
+/*
+Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+last2("hixxhi") → 1
+last2("xaxxaxaxx") → 1
+last2("axxxaaxx") → 2*/
+public static int last2(String str) {
+
+  
+  
+}
+
 
 public static void main(String[] args){
 
@@ -119,10 +151,16 @@ public static void main(String[] args){
     System.out.println(doubleX("xxxxxx"));
 
     //stringBits
-    
 
+    System.out.println(stringBits("Hello"));
+    System.out.println(stringBits("Hi"));
+    System.out.println(stringBits("Heeololeo"));
 
+    //stringSplosion
 
+    System.out.println(stringSplosion("Code"));
+    System.out.println(stringSplosion("abc"));
+    System.out.println(stringSplosion("ab"));
 
 
 

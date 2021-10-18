746c543500d67379ab5a09de1e0019bbd0f9ebc3
meghrxj <hello.meghrajjj@gmail.com>
2021-10-18 10:24:54 +0530

    Update warmup2.java

--- WarmpUp2/warmup2.java
+++ WarmpUp2/warmup2.java
@@ -1,5 +1,32 @@
 package WarmpUp2;
 
-public class warmup2 {
+public class warmup2 
+{
+
+/*Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+stringTimes("Hi", 2) → "HiHi"
+stringTimes("Hi", 3) → "HiHiHi"
+stringTimes("Hi", 1) → "Hi" */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 }

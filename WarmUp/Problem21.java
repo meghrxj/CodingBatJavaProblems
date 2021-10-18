6668e1944dc621d93a53f5a612e4cdee57e6ce64
meghrxj <hello.meghrajjj@gmail.com>
2021-10-18 09:59:52 +0530

    Update Problem 21, Added Problem 22

--- WarmUp/Problem21.java
+++ WarmUp/Problem21.java
@@ -4,11 +4,20 @@ public class Problem21 {
 
     public static String delDel(String str){
 
-        
+        if (str.length()>=4 && str.substring(1, 4).equals("del")) {
+            // First char + rest of string starting at 4
+            return str.substring(0, 1) + str.substring(4);
+          }
+          // Otherwise return the original string.
+          return str;
+
     }
 
     public static void main(String[] args){
 
+        System.out.println(delDel("adelbc"));
+        System.out.println(delDel("adelHello"));
+        System.out.println(delDel("adedbc"));
     }
     
 }

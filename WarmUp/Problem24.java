c4f5b260dd95a17546c3699257b077d1edabc7f8
meghrxj <hello.meghrajjj@gmail.com>
2021-10-18 10:12:14 +0530

    Problem 23 - 25

---
+++ WarmUp/Problem24.java
@@ -0,0 +1,29 @@
+package WarmUp;
+
+public class Problem24 {
+
+    public static int intMax(int a, int b, int c){
+
+        int max;
+
+        if(a > b){
+            max = a;
+        } else {
+            max = b; } 
+        
+        if(c > max){
+            max = c;
+        } 
+
+        return max;
+
+    }
+    public static void main(String[] args){
+
+        System.out.println(intMax(1, 2, 3));
+        System.out.println(intMax(1, 3, 2));
+        System.out.println(intMax(3, 2, 1));
+
+    }
+    
+}

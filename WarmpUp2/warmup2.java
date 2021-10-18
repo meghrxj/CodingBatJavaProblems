80f2165daaa4f79ff82518da6ab721974438ebfb
meghrxj <hello.meghrajjj@gmail.com>
2021-10-18 16:20:04 +0530

    Update warmup2.java

--- WarmpUp2/warmup2.java
+++ WarmpUp2/warmup2.java
@@ -175,6 +175,22 @@ public static boolean arrayFront9(int[] nums) {
   
 }
 
+/*Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+array123([1, 1, 2, 3, 1]) → true
+array123([1, 1, 2, 4, 1]) → false
+array123([1, 1, 2, 1, 2, 3]) → true*/
+public static boolean array123(int[] nums) {
+
+    boolean result = false;
+
+    for(int i = 0; i < nums.length - 2; i++) {
+      if(nums[i] == 1 && nums[i + 1] == 2 && nums[i + 2] == 3){
+        result = true;
+      }
+    }
+    return result;
+  
+}
 
 public static void main(String[] args){
 

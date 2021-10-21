981ccb89acbd1e2f889b1f75c68ff672ef8ec66c
meghrxj <hello.meghrajjj@gmail.com>
2021-10-21 18:26:45 +0530

    Update warmup2.java

--- WarmpUp2/warmup2.java
+++ WarmpUp2/warmup2.java
@@ -198,6 +198,26 @@ stringMatch("xxcaazz", "xxbaaz") → 3
 stringMatch("abc", "abc") → 2
 stringMatch("abc", "axc") → 0*/
 
+public int stringMatch(String a, String b){
+
+    int count = 0;
+
+    if(a.length() >= 2 && b.length() >= 2){
+
+      for(int i = 0; i < a.length() - 1 && i < b.length() - 1; i++){
+          if(a.substring(i, i + 2) == b.substring(i, i + 2)){
+
+            count++;
+          }
+
+          
+      }
+    }
+    return count;
+}
+
+
+
 
 
 

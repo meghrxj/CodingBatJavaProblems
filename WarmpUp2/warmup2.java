e406b7c5b5729fc83939380a9e60809ae1a77c97
meghrxj <hello.meghrajjj@gmail.com>
2021-10-21 22:15:02 +0530

    Update warmup2.java

--- WarmpUp2/warmup2.java
+++ WarmpUp2/warmup2.java
@@ -216,7 +216,16 @@ public int stringMatch(String a, String b){
     return count;
 }
 
+/*
+Given a string, return a version where all the "x" have been removed. Except an "x" at the very start or end should not be removed.
+stringX("xxHxix") → "xHix"
+stringX("abxxxcd") → "abcd"
+stringX("xabxxxcdx") → "xabcdx"*/ 
 
+public String stringX(String str) {
+  
+  
+}
 
 
 
